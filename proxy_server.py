214f00411e48dd53fae2eb802da9fed442b76a7a
Joseph Zhang <jza334@sfu.ca>
2025-10-28 21:00:49 -0700

    finished basic implementation of proxy server

--- proxy_server.py
+++ proxy_server.py
@@ -21,24 +21,24 @@ class Cache:
         self.keyNum = 0
 
     # This function takes in a request, and searches to see if the request is in the cache. If 
-    # the request is in the cache, the LRU indicator is updated, then the object is returned.
+    # the request is in the cache, the LRU indicator is updated, then the content is returned.
     # Otherwise, None is returned.
     def check_cache(self, target):
         for key, value in self.cache.items():
-            request, object = value
+            request, content = value
             if target == request:
-                self.cache.pop(key)
-                self.cache[self.keyNum] = (request, object)
-                self.keyNum += 1
-                return object
+                print("Cache hit!\n")
+                self._replace(key, request, content)
+                return content
+        print("Cache Miss!\n")
         return None
     
-    # This functions takes in a request and a object, and stores it in the cache. If the cache
+    # This functions takes in a request and a content, and stores it in the cache. If the cache
     # is full, the least recently used element is evicted. 
-    def set(self, request, object):
+    def set(self, request, content):
         if (len(self.cache) >= self.max_size):
             self._evict()    
-        self.cache[self.keyNum] = (request, object)
+        self.cache[self.keyNum] = (request, content)
         self.keyNum += 1
 
     def _evict(self):
@@ -46,13 +46,34 @@ class Cache:
         min_key = min(keys)
         self.cache.pop(min_key)
 
+    # This function takes a key, request and content, and updates the LRU value for the entry.
+    def _replace(self, key, request, content):
+        request, content = self.cache.pop(key)
+        self.set(request, content)
+
+    # This function takes in a target request, and searches through the cache to try and find a 
+    # request that is *matching*. In this context, matching refers to having the same values 
+    # without the headers. If a match is found, the LRU value is updated. Otherwise, the request
+    # is added to the cache. 
+    def replace304(self, target, tContent):
+        replaced = False
+        for key, value in cache.cache.items():
+            request, content = value
+            stripped = request.split("\n")[0]
+            if stripped == target:
+                self._replace(key, target, content)
+                replaced = True
+                break
+        if replaced == False:
+            self.set(target, tContent)
+
     # for debugging
     def print(self):
         print(f"Cache size: {len(self.cache)}")
         for key, value in self.cache.items():
             if isinstance(value, tuple) and len(value) == 2:
-                request, object = value
-                print(f"{key}: \n{request} \n{object} \n")
+                request, content = value
+                print(f"{key}: \n{request} \n{content} \n")
             else:
                 print(f"Unexpected value format for key {key}: {len(value)}")
 
@@ -78,7 +99,7 @@ while True:
     # Convert byte form
     request_string = request.decode("utf-8")
 
-    #parse request
+    # parse request
     lines = request_string.split("\n")
     request_line = lines[0].split()
     if len(request_line) < 3:
@@ -87,32 +108,38 @@ while True:
     method, path, version = request_line
     headers = {l.split(": ")[0]: l.split(": ")[1] for l in lines[1:] if ": " in l} #make dictionary for easy access to hearer info
 
-    #505 logic
+    # 505 logic
     if version != "HTTP/1.1":
         response = "HTTP/1.1 505 HTTP Version Not Supported\n\n"
     elif "If-Modified-Since" in headers:
         response = getServerResponse(request)
+        response_string = response.decode("utf-8")
 
+        target = request_string.split("\n")[0]
+        content = response_string.split("\n\n",1)[1]
+        
+        cache.replace304(target, content)
     else:
-        response_object = cache.check_cache(request_string)
-        if response_object:
-            if isinstance(response_object, str):
-                response_string = "HTTP/1.1 200 OK\n\n" + response_object
-                response = response_string.encode("utf-8")
+        response_content = cache.check_cache(request_string)
+        if response_content:
+            if isinstance(response_content, str):
+                response_string = "HTTP/1.1 200 OK\n\n" + response_content
         else:
             response = getServerResponse(request)
 
             response_string = response.decode("utf-8")
-
             response_code = response_string.split("\n")[0].split(" ")[1]
 
             if response_code in ["403", "404", "505"]:
                 pass
-            elif response_code in ["304", "200"]:
-                content = response_string.split("\n\n")[1]
+            elif response_code == "200":
+                content = response_string.split("\n\n",1)[1]
                 cache.set(request_string, content)
 
+    
     if isinstance(response, str):
         response = response.encode("utf-8")
+    print(response.decode("utf-8"))
     client_conn.sendall(response)
     client_conn.close()
+    
--- web_server.py
+++ web_server.py
@@ -42,6 +42,8 @@ while True:
             response = "HTTP/1.1 403 Forbidden\n\n"
         #304 logic
         elif "If-Modified-Since" in headers:
+            with open(filepath, 'r') as file:
+                content = file.read()
             client_time = parsedate_to_datetime(headers["If-Modified-Since"]).timestamp()
             file_time = os.path.getmtime(filepath)
             if file_time <= client_time:
